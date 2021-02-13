e7de0e1885f1443b115a50b46196f53784b6da48
destroyerstv <destroyer.86@mail.ru>
2021-02-13 21:56:36 +0300

    Delete SQLProfitCenter.sql

--- SQLProfitCenter.sql
+++
@@ -1,3 +0,0 @@
-
-
-select name from (select worker.name, tbl1.worker_id from worker right join (select worker_id from money where MONTH(date) = 12) as tb1 on worker.id = tbl1.worker_id) as tbl2 where worker_id is null
